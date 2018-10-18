0b1ef79d3ab080bf52af740e1eb2f999a9bc98bf
martin-hrach <martin-hrach@centrum.cz>
2018-10-18 04:44:15 +0200

    skoro hotovo

--- index.js
+++ index.js
@@ -1,88 +1,107 @@
 const app = require('express')();
-require('util').inspect.defaultOptions.depth = null
+//const player = require('play-sound')(); přidat až ke konci projectu
+const fs = require('fs');
 const path = require("path");
 const https = require("https");
 const ejs = require('ejs');
 const cmd = require('node-cmd');
 const lineReader = require('readline').createInterface({
-	input: require('fs').createReadStream('test.txt')
+	input: fs.createReadStream('test.txt')
 });
 
-
+const dirpath = path.join(__dirname, './twitchChat') // get=> všechny soubory ve složce twitchChat skoncovkou .txt
 const streamers = ["grimmmz","smoke","kotton","nl_kripp"];
 const allResponse = [];
-app.locals.allResponse = allResponse
+//app.locals.allResponse = allResponse
 
-streamers.forEach((streamer,i) => {
+// loop request o videa zadaných streamerů
+streamers.forEach(streamer => {
 	https.get({
-		hostname: 'api.twitch.tv',
-		path: '/kraken/channels/'+streamer+'/videos?broadcast_type=archive',
+		hostname: "api.twitch.tv",
+		path: `/kraken/channels/${streamer}/videos?broadcast_type=archive`,
 		method: 'GET',
 		headers: {
 			'Client-ID': '3gx16c6mkdnv928hqo7y8rbgdke6c3',
 			'Authorization': 'OAuth ij3hiav0u7nn7v1shrsy38xacsn78e'
 							}
 		}, function(res) {
-				let info = ""
+				let info = "";
+
 			res.on('data', function(dataFlow) {
 				info += dataFlow
-		});
+			});
+
 			res.on('end', function() {
-				let d = new Date()
- 			  let datestring = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate()
+				const d = new Date();
+ 			  const datestring = `${d.getFullYear()} - ${d.getMonth()+1} - ${d.getDate()}`;
+
         allResponse.push(JSON.parse(info).videos.filter(a => a.published_at.slice(0,10) == "2018-10-14"))
-			  //tahle část je funkční, filtruje requesty a ukládá jen data o videích která byla
-			  //publikovaná v určeném datu
-	      if(i == streamers.length-1) { reqcom() } //tady neco nesedí bude nutné předělat
-	  });
-	})
+			  //tahle část je funkční, filtruje requesty a ukládá jen data o videích která byla publikovaná v určeném datu
+	      if(allResponse.length === streamers.length) { requestsComplet() } //čekání na vyplnění všech requestů a následné volání funkce
+	  	});
+		})
 });
 
-function reqcom() { console.log("test uspešný: " + allResponse.length)
- 										console.log(JSON.stringify(allResponse[1], null, 2))
-}
+function requestsComplet() {
+		const videosID = [].concat(...allResponse).map(a => a._id.slice(1))
 
+		console.log(`počet objektů v array: ${allResponse.length}`)
+		//console.log(JSON.stringify(allResponse[3], null, 2))
+		//console.log(JSON.stringify(videosID, null, 2))
+		cmdDonwload(videosID);
+};
 
+const testRes = [] // ukládání reponse pro zajištění podmínky při čekání na vyplnení úkolů
 
-
-
-var test = ["321144245","321144058","323320304"]
-
-cmd.run(console.log(`startuje stahovaní chetů, celkový počet: ${test.length-1}`))
-test.forEach((a,i) => cmd.get(` cd.. & cd twitchChat & rechatTool -D ${a}`,
-	function(err, data, stderr){
-					if(!err) {console.log("žádný problém nenalezen")}
-					console.log(`stažení souboru ${i} dokončeno`,stderr)
-					if(i == test.length-1) { done(i+1); }
-			})
-		)
+function cmdDonwload(videosID) {
+	cmd.run(console.log("Spouštím stahovnání...\n"))
+	videosID.forEach((a,i) => cmd.get(`cd twitchChat & rechatTool -D ${a}`,
+		function(err, data, stderr){
+						if(err) {console.log(`tento soubor již exituje`)}
+						testRes.push(data)
+						console.log(`stažení souboru ${i+1} z ${videosID.length} dokončeno`)
+						if(testRes.length === videosID.length) { done(videosID.length); };
+		})
+	)
+};
 // logika dokončena, spustí stahování chatu cílového streamu po dokončení zavolá callback
 // po dokončení stahování všech souborů zavola funcki done
-function done(info) { console.log(`Všechny úspěšně dokončeno, celkem staženo: ${info}`) }
 
+function done(info) {
+	fs.readdir(dirpath, function(err, files) {
+		const txtFiles = files.filter(el => /\.txt$/.test(el))
+		console.log(txtFiles)
+	});
+	console.log(`Všechny soubory úspěšně zpracovány, celkem staženo: ${info}`)
 
+	/*player.play('./mech.mp3', (err) => {
+		if (err) console.log(`Could not play sound: ${err}`); nainstalovat mpplayer a pak nastavit path pro windows:/
+	});*/
+};
+
+// =================== ZDE JE PROZATÍM VŠE DOKONČENO ==================================
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
 /*** variables ***/
-let timeArr = [];
-let lulWords = [];
-let sadWords = [];
+const timeArr = [];
+const lulWords = [];
+const sadWords = [];
 app.locals.timeArr = timeArr;
 app.locals.lulWords = lulWords;
 app.locals.sadWords = sadWords;
 
 let time = 0;
 let lul = 0;
-let sad = 0
+let sad = 0;
 /****************/
 
 lineReader.on('line', function (line) {
-	let reg = /\[(.*?)\]/,  lookingForLul = (/\b lul \b|lol|lool|haha|hahaa|omegalul|omegalol|lmao|rofl|omfg|lmfao|ez|pog|clap|wow|poggers|kreygasm|rekt|clip /);
-	let lookingForSad = (/\b biblethump \b|notlikethis|pepehands|sad|cry|rip|feelswierman|feelsbadman|monkas|pepewhy /);
-	let item = line.match(reg);
-	let allSmall = line.toLowerCase();
-	;
+	const reg = /\[(.*?)\]/;
+	const lookingForLul = (/\b lul \b|gachigasm|lol|lool|haha|hahaa|omegalul|omegalol|lmao|rofl|omfg|lmfao|ez|pog|clap|wow|poggers|pogchamp|hypers|kreygasm|elegiggle|4head|rekt|clip /);
+	const lookingForSad = (/\b biblethump \b|failfish|babyrage|notlikethis|pepehands|sad|cry|rip|feelswierman|feelsbadman|monkas|pepewhy|dansgame /);
+	const item = line.match(reg);
+	const allSmall = line.toLowerCase();
 
 	if(allSmall.match(lookingForSad)) { sad += 1
 		//console.log("sad : " +sad)
@@ -91,11 +110,11 @@ lineReader.on('line', function (line) {
     if(allSmall.match(lookingForLul)) { lul += 1 }
 
 	if(item[1].length === 12) { //target controlor
-		let splitedItem = item[1].slice(0,8).split(":");
-		let convertTime = (+splitedItem[0]) * 60 * 60 + (+splitedItem[1]) * 60 + (+splitedItem[2]);
+		const splitedItem = item[1].slice(0,8).split(":");
+		const convertTime = (+splitedItem[0]) * 60 * 60 + (+splitedItem[1]) * 60 + (+splitedItem[2]);
 		let date = new Date(null);
-			date.setSeconds(convertTime-30); // specify value for SECONDS here
-    	let result = date.toISOString().substr(11, 8);
+				date.setSeconds(convertTime-30) // specify value for SECONDS here
+    const result = date.toISOString().substr(11, 8);
 
 		if ( (convertTime - time) >= 60 ) { //vložit do array každou časovou jednotku a vynulovat počty nalezených slov
 			timeArr.push(result)
@@ -106,7 +125,7 @@ lineReader.on('line', function (line) {
 			sad = 0;
 			time = convertTime;
 		}
-    }
+  }
 	//console.log('Line from file:', allSmall);
 });
 
@@ -117,9 +136,8 @@ lineReader.on('close', function (data) {
 
 app.get("/", function(req,res){
 	res.render("index")
-})
-;
+});
+
 app.listen(3000, function () {
-console.log("Server is running on port mops");
-console.log("allResponse: " + allResponse.length)
+console.log("mops server je nastartován \n");
 });
